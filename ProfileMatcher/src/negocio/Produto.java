82a448636ab0e630a0c2672ca971109bd9643ee0
Herick Oliveira <hkmarques@gmail.com>
2012-06-30 11:15:04 -0300

    Adicionando classes Produto e PalavrasChave.

---
+++ ProfileMatcher/src/negocio/Produto.java
@@ -0,0 +1,102 @@
+package negocio;
+
+public class Produto {
+
+    private int codigo;
+
+    private PalavrasChave palavrasChave;
+
+    private float preco;
+
+    private String tamanho;
+
+    private String cor;
+
+    private String tipo;
+
+    private String descricao;
+
+    private String marca;
+
+    // ----------------------------------------------------------------------------------------------------------------
+    // ----------------------------------------------------------------------------------------------------------------
+    // ------------------------------------------ CONSTRUCTORS --------------------------------------------------
+    // ----------------------------------------------------------------------------------------------------------------
+    // ----------------------------------------------------------------------------------------------------------------
+
+    public Produto(int codigo) {
+        this.codigo = codigo;
+        this.palavrasChave = new PalavrasChave();
+    }
+
+    // ----------------------------------------------------------------------------------------------------------------
+    // ----------------------------------------------------------------------------------------------------------------
+    // ----------------------------------------------------------------------------------------------------------------
+    // ----------------------------------------------------------------------------------------------------------------
+    // ----------------------------------------- GETTERS / SETTERS ----------------------------------------------------
+    // ----------------------------------------------------------------------------------------------------------------
+    // ----------------------------------------------------------------------------------------------------------------
+    // ----------------------------------------------------------------------------------------------------------------
+    // ----------------------------------------------------------------------------------------------------------------
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public PalavrasChave getPalavrasChave() {
+        return palavrasChave;
+    }
+
+    public void setPalavrasChave(PalavrasChave palavrasChave) {
+        this.palavrasChave = palavrasChave;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+
+    public String getTamanho() {
+        return tamanho;
+    }
+
+    public void setTamanho(String tamanho) {
+        this.tamanho = tamanho;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+}
